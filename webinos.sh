2b38275869e5fd2f194b511c0e82fb45f4933c80
Christian Fuhrhop <christian.fuhrhop@fokus.fraunhofer.de>
2012-05-15 15:48:38 +0200

    Removed 'raises' from defintions. Moved W3C contacts to contacts

--- webinos.sh
+++ webinos.sh
@@ -108,7 +108,7 @@ DELIM
 
 for i in $WIDLFILES
 do
-    if [ "$i" != "foo.widl" ] && [ "$i" != "contacts.widl" ] && [ "$i" != "calendar.widl" ] && [ "$i" != "gallery.widl" ]  ; 
+    if [ "$i" != "foo.widl" ] && [ "$i" != "calendar.widl" ] && [ "$i" != "gallery.widl" ]  ; 
     then
 	basename2=$(basename "$i" .widl)
 	htmlfile="$SPECHOME/apis/"$basename2".html"
@@ -125,8 +125,6 @@ echo '<div id="content"><table><thead><tr><th>Specification</th><th>Summary</th>
 
 echo '<tr><td><a href=calendar.html>The W3C calendar module</a></td><td><p>' >> "$SPECHOME/apis/index.html"
 echo 'This W3C API provides access to a user calendaring service.<br/></p></td></tr>' >> "$SPECHOME/apis/index.html"
-echo '<tr><td><a href=contacts.html>The W3C contacts module</a></td><td><p>' >> "$SPECHOME/apis/index.html"
-echo 'This W3C API provides access to a user unified address book.<br/></p></td></tr>' >> "$SPECHOME/apis/index.html"
 echo '<tr><td><a href=devicestatus.html>The WAC devicestatus module</a></td><td><p>' >> "$SPECHOME/apis/index.html"
 echo 'This WAC API provides access to the information about the device status. The status information is organised as a tree structure utilising a vocabulary. <br/></p></td></tr>' >> "$SPECHOME/apis/index.html"
 echo '<tr><td><a href=vocabulary.html>The devicestatus vocabulary module</a></td><td><p>' >> "$SPECHOME/apis/index.html"
